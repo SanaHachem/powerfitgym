c24d5f01839c66b9b0710fd0fd136476aa5ae2f8
diegosoutofreitas <diego.freitas@olxbr.com>
2025-09-20 16:54:45 -0300

    Adicionado, Header, CSS e JS

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('#btn-contact') 
+btn.addEventListener('click', () => {
+    const sectionPrincipal = document.querySelector('#plano-principal')
+    sectionPrincipal.scrollIntoView()
+})
