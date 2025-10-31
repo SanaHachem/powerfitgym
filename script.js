d0dabfc6001a34900b3317a91628fcc56365e56e
SanaHachem <sana.hachem@fatec.sp.gov.br>
2025-10-31 20:29:59 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector('#btn-contact') 
-btn.addEventListener('click', () => {
-    const sectionPrincipal = document.querySelector('#plano-principal')
-    sectionPrincipal.scrollIntoView()
-})
